94e49e75ee24123da911c88db8c9c036d5b3d024
Paul Torka <paul.torka@peerigon.com>
2015-03-17 20:28:51 +0100

    Push current state

---
+++ client/actions/message.js
@@ -0,0 +1,6 @@
+"use strict";
+
+exports.DRAFT = "message draft";
+exports.SEND = "message send";
+exports.DELIVERED = "message delivered";
+exports.READ = "message read";
--- client/stores/MessageStore.js
+++ client/stores/MessageStore.js
@@ -1,2 +1,34 @@
 "use strict";
 
+var AppDispatcher = require("../dispatcher/AppDispatcher.js");
+var EventEmitter = require("events");
+var message = require("../actions/message.js");
+
+var handlers = {};
+var messages = {};
+var MessageStore = new EventEmitter();
+var nextTempId = 0;
+
+function getNextTempId() {
+    return "message-temp-" + nextTempId++;
+}
+
+handlers[message.SEND] = function (action) {
+    messages[getNextTempId()] = {
+
+    };
+};
+
+MessageStore.EVENTS = {
+    DRAFT: "draft"
+};
+
+MessageStore.dispatchToken = AppDispatcher.register(function (action) {
+    var handler = handlers[action.actionType];
+
+    if (handler) {
+        handler(action);
+    }
+});
+
+module.exports = MessageStore;
